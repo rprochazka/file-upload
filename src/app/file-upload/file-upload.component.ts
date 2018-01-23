be7af1dc17b1b84011d9c783aedbab93dd36c946
Radek Prochazka <radek_prochazka@seznam.cz>
2018-01-23 11:53:37 +0100

    fixing the file order assignmebt

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -69,8 +69,13 @@ export class FileUploadComponent implements OnInit {
 
   onUpload() {
     const fileItems = this.fileItems.map((item, index) => {
-      item.metadata.order = index + 1;
-      return item;
+      return {
+        ...item,
+        metadata: {
+          ...item.metadata,
+          order: index
+        }
+      }
     });
     console.log(fileItems);
     Observable.from(fileItems)
